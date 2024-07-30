1df04f55bfc3fd5645bd5688b9c5db26b71cc41f
Shahwar-Alam-Naqvi <117357531+Shahwar-Alam-Naqvi@users.noreply.github.com>
2024-07-31 01:33:53 +0530

    OOP Properties

---
+++ 02_OOP_Properties.py
@@ -0,0 +1,20 @@
+class Animal:
+    def speak(self):
+        pass
+    
+class Dog(Animal):
+    def speak(self):
+        return "Bark"
+    
+class Cat(Animal):
+    def speak(self):
+        return "Meow"
+
+def make_animal_speak(animal):
+    print(animal.speak())
+    
+dog = Dog()
+cat = Cat()
+
+make_animal_speak(dog)
+make_animal_speak(cat)
