f8372824362b0f0cb24e76ac93bf162f363378b1
Shahwar-Alam-Naqvi <117357531+Shahwar-Alam-Naqvi@users.noreply.github.com>
2024-08-01 00:45:45 +0530

    constructor

---
+++ 05_Constructor.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self, name, roll_number):
+        self.name = name
+        self.roll_number = roll_number
+
+student1 = Student("Shahwar", 47)
+print(student1.name)  # Output: Shahwar
+print(student1.roll_number)  # Output: 47
